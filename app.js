9c29fec80f4c02c74bbe5a3ca3e4d76116d23fd0
Michael Schienbein <moomba0@gmail.com>
2020-10-20 20:06:00 +0300

    fixed bottom text label, improved generateImg()

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-let memeForm = document.querySelector('.meme-form');
-let imgBox = document.querySelector('.imgBox');
+let memeForm = document.querySelector(".meme-form");
+let imgBox = document.querySelector(".imgBox");
 
-memeForm.addEventListener('submit',(e) => {
-    let urlInput = document.querySelector('.url-input');
-    let topInput = document.querySelector('.top-input');
-    let bottomInput = document.querySelector('.bottom-input');
-    e.preventDefault();
-    if (urlInput.value === ''){
-        return;
-    }
-    generateImg(urlInput.value,topInput.value,bottomInput.value);
+memeForm.addEventListener("submit", (e) => {
+	let urlInput = document.querySelector(".url-input");
+	let topInput = document.querySelector(".top-input");
+	let bottomInput = document.querySelector(".bottom-input");
+	e.preventDefault();
+	if (urlInput.value === "") {
+		return;
+	}
+	generateImg(urlInput.value, topInput.value, bottomInput.value, imgBox);
 });
 
-imgBox.addEventListener('click', (e) => {
-    let del = e.target;
-    let top = document.querySelector('.top');
-    let bottom = document.querySelector('.bottom');
-    if (del === top || bottom){
-        bottom.parentElement.remove();
-        console.log(del);
-    }
+imgBox.addEventListener("click", (e) => {
+	let del = e.target;
+	let top = document.querySelector(".top");
+	let bottom = document.querySelector(".bottom");
+	if (del === top || bottom) {
+		bottom.parentElement.remove();
+		console.log(del);
+	}
 });
 
-function generateImg(url,top,bottom) {
-    let newImg = document.createElement('img');
-    let newImgDiv = document.createElement('div');
-    let topText = document.createElement('div');
-    let bottomText = document.createElement('div');
-    topText.classList.add('top');
-    topText.innerText = top;
-    bottomText.classList.add('bottom');
-    bottomText.innerText = bottom;
+function generateImg(url, top, bottom, box) {
+	let newImg = document.createElement("img");
+	let newImgDiv = document.createElement("div");
+	let topText = document.createElement("div");
+	let bottomText = document.createElement("div");
+	topText.classList.add("top");
+	topText.innerText = top;
+	bottomText.classList.add("bottom");
+	bottomText.innerText = bottom;
 
-    newImg.setAttribute('src', url);
-    newImgDiv.classList.add('img');
-    newImgDiv.appendChild(newImg);
-    newImgDiv.appendChild(topText);
-    newImgDiv.appendChild(bottomText);
-    imgBox.appendChild(newImgDiv);
-};
+	newImg.setAttribute("src", url);
+	newImgDiv.classList.add("img");
+	newImgDiv.appendChild(newImg);
+	newImgDiv.appendChild(topText);
+	newImgDiv.appendChild(bottomText);
+	box.appendChild(newImgDiv);
+}
